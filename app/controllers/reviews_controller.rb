f22b79b9c4d80fb133be92c034a6325b43ed7d07
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-22 18:29:06 +0100

    not done yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,8 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, :only[:new, :create]
+
   def index
+    @review = Review.new
   end
 
   def show
@@ -9,5 +12,23 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+    redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
 end
