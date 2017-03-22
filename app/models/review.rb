65bf3d8064b60ed682045f2b542aca61fd22c1e5
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-22 17:13:47 +0100

    half way there

---
+++ app/models/review.rb
@@ -0,0 +1,11 @@
+class Review < ApplicationRecord
+  # validates :content
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+
+  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
+  validates :rating, numericality: { only_integer: true }
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create( name: 'Star Wars' },  name: 'Lord of the Rings' )
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create( name: 'The Bucher', address: "Albert Cuypstraat 129", phone_number: "020 470 7875", category: "french")
+Restaurant.create( name: 'le petit', address: "Nieuwezijds Voorburgwal 306, 1012 Amsterdam", phone_number: "020 624 9425", category: "belgian")
+Restaurant.create( name: 'Mata Hari', address: " Oudezijds Achterburgwal 22, 1012 DM Amsterdam", phone_number: "020 205 0919", category: "italian")
+Restaurant.create( name: 'De Waterkant', address: "Marnixstraat 246, 1016 TL Amsterdam", phone_number: "020 737 1126", category: "japanese")
